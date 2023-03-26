1bf0099c7d5390729d326cb0113ca489024a87d1
m-njora <mmuthekijora@gmail.com>
2023-03-26 04:55:49 +0300

    added todo page

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,7 @@
-// import React, {useState} from 'react'
+import React, {useState} from 'react'
 
 
-// function Todo() {
+function Todo() {
 //     const [Todos, setTodos] = useState([]);
 //     const [newTodo, setNewTodo] = useState('');
 //     const [editingIndex, setEditingIndex] = useState(null);
@@ -37,45 +37,45 @@
 //         setEditingIndex(null);
 //       };
     
-//     return (
-//         <div className='form'>
-//         <h2>My Todos </h2>
-//         <form onSubmit={handleAddTodo}>
-//           <label htmlFor="new-Todo">New Todo:</label>
-//           <input
-//             type="text"
-//             id="new-Todo"
-//             value={newTodo}
-//             onChange={handleNewTodoChange}
-//           />
-//           <button type="submit">{editingIndex !== null ? 'Save Todo' : 'Add Todo'}</button>
-//         </form>
-//         {Todos.length > 0 ? (
-//           <ul>
-//             {Todos.map((Todo, index) => (
-//               <li key={index}>
-//                 {editingIndex === index ? (
-//                   <input type="text" value={newTodo} onChange={handleNewTodoChange} />
-//                 ) : (
-//                   <>{Todo}</>
-//                 )}
-//                 {editingIndex === index ? (
-//                   <button onClick={() => setEditingIndex(null)}>Cancel</button>
-//                 ) : (
-//                   <>
-//                     <button onClick={() => handleEditTodo(index)}>Edit</button>
-//                     <button onClick={() => handleDeleteTodo(index)}>X</button>
-//                   </>
-//                 )}
-//               </li>
-//             ))}
-//           </ul>
-//         ) : (
-//           <p>No Todos added yet</p>
-//         )}
-//       </div>
-//     );
+    return (
+        <div className='form'>
+        <h2>My Todos </h2>
+        <form onSubmit={handleAddTodo}>
+          <label htmlFor="new-Todo">New Todo:</label>
+          <input
+            type="text"
+            id="new-Todo"
+            value={newTodo}
+            onChange={handleNewTodoChange}
+          />
+          <button type="submit">{editingIndex !== null ? 'Save Todo' : 'Add Todo'}</button>
+        </form>
+        {Todos.length > 0 ? (
+          <ul>
+            {Todos.map((Todo, index) => (
+              <li key={index}>
+                {editingIndex === index ? (
+                  <input type="text" value={newTodo} onChange={handleNewTodoChange} />
+                ) : (
+                  <>{Todo}</>
+                )}
+                {editingIndex === index ? (
+                  <button onClick={() => setEditingIndex(null)}>Cancel</button>
+                ) : (
+                  <>
+                    <button onClick={() => handleEditTodo(index)}>Edit</button>
+                    <button onClick={() => handleDeleteTodo(index)}>X</button>
+                  </>
+                )}
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No Todos added yet</p>
+        )}
+      </div>
+    );
       
-// }
+}
 
-// export default Todo
+export default Todo
