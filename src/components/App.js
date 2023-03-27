d624c17dcd10af8f307afeede914589737f7f72d
m-njora <mmuthekijora@gmail.com>
2023-03-27 09:14:34 +0300

    fixed routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,36 @@
-// import './App.css';
+import React from 'react'
 import Login from './login';
-import SignUp from "./signup";
+// import SignUp from "./signup";
 import Todo from "./Todo"
-import { Route, Switch, Redirect } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 function App() {
+  // const [loggedIn, setLoggedIn] = React.useState(false);
+  // const [userName, setUserName] = React.useState('');
+
+
+  // function handleLogin(name){
+  //       // Set the loggedIn state to true after successful login
+
+  //   setLoggedIn(true);
+  //   setUserName(name);
+
+  // }
+  // function handleLogout(){
+  //   // Set the loggedIn state to false after successful logout
+  //   setLoggedIn(false);
+  //   setUserName('');
+
+  // }
+
   return (
 <Switch>
 
+  <Route path="/todo" component={Todo} />
   <Route path="/login" component={Login} />
-  <Route path="/signup" component={SignUp} />
-  <Redirect from="/" to="/login" />
-  <Route path="/" component={Todo} />
 
+
+  {/* <Route path="/login" component={Login}  onLogin={handleLogin}/> */}
+  {/* <Route path="/todo" render={() => <Todo userName={userName} onLogout={handleLogout} />}  />  */}
 </Switch>
 
   );
